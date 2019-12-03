8853185a7bec7b52e06842a2291d0ecb430a55fa
raphael <rtnanje@gmail.com>
2019-12-03 01:02:22 -0500

    fix producer.results.append bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='easyasyncio',
-    version='4.0.3',
+    version='4.0.4',
     packages=['easyasyncio'],
     url='https://github.com/RaphaelNanje/easyasyncio.git',
     license='MIT',
