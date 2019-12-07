eef3bc4a30f287bea6d7a26c5c13a64c491f76a9
raphael <rtnanje@gmail.com>
2019-12-07 06:17:29 -0500

    fix increment_stat bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='easyasyncio',
-    version='5.5.5',
+    version='5.5.6',
     packages=['easyasyncio'],
     url='https://github.com/RaphaelNanje/easyasyncio.git',
     license='MIT',
