d73ea625311894cf1553bf0cbc213e6d251c5d15
raphael <rtnanje@gmail.com>
2019-12-10 02:51:47 -0500

    add datamanager test

---
+++ tests/test_datamanager.py
@@ -0,0 +1,32 @@
+import shutil
+
+import pytest
+
+from easyasyncio import LoopManager
+
+manager = LoopManager()
+data = manager.context.data
+
+shutil.rmtree('./testfiles/')
+
+
+@pytest.mark.asyncio
+async def test_registration():
+    d = [[f'test{i}', f'data{i}', f'row{i}'] for i in range(100)]
+    headers = 'test,data,row'.split(',')
+    data.register('csv', d, './testfiles/test.csv',
+                  save_kwargs=dict(headers=headers))
+    data['csv'].append('8,9,10'.split(','))
+    print(data['csv'])
+    await data.save()
+
+    load = data.filemanager.load('csv')
+    print(load)
+    assert 'test,data,row' in load
+
+
+@pytest.mark.asyncio
+async def test_loading():
+    data.register('csv', list(), './testfiles/test.csv')
+    load = data.get('csv')
+    assert 'test0,data0,row0' in load
