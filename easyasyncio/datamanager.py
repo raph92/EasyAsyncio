60abf60d7b88f2acf2ea134879d958200a10ee8e
raphael <rtnanje@gmail.com>
2019-12-23 15:22:38 -0500

    add cache support to datamanager.py
    new functions: clean(), register_cache(***)

--- easyasyncio/datamanager.py
+++ easyasyncio/datamanager.py
@@ -3,7 +3,10 @@ import os
 from collections import UserDict
 from typing import Sized, Iterable, Dict
 
+from diskcache import Index, Deque
 from easyfilemanager import FileManager
+
+from easyasyncio.cachetypes import CacheSet
 
 
 def _numericize(loaded_data):
@@ -26,7 +29,7 @@ def _numericize(loaded_data):
 class DataManager(UserDict):
     filemanager = FileManager()
     directory = '.'
-    do_not_display = []  # data items not to show
+    do_not_display = []  # data items not to show in StatDisplay
     save_kwargs: 'Dict[str, Dict]' = dict()
     load_kwargs: 'Dict[str, Dict]' = dict()
 
@@ -67,6 +70,69 @@ class DataManager(UserDict):
         if loaded_data:
             self.load(initial_data, loaded_data, name)
 
+    def register_cache(self, name, data_type, path_to_file=None,
+                       display=True, load=True, save_kwargs: dict = None,
+                       load_kwargs: dict = None) -> None:
+        """
+        The purpose of this function is to avoid having all objects loaded
+        in memory and instead use a diskcache for storing/accessing objects.
+
+        This function will create a cache at './cache/**name**' and register
+        the file passed to `path_to_file` for autosaving.
+
+        If `path_to_file` is None, then data will only be saved to the cache
+
+        This data will be accessed by `JobManager.context.data.get(name)` and
+        will return one of the following: :class:`Index`, :class:`CacheSet`,
+        or :class:`Deque`.
+
+        This data will be saved using ``save()``
+        Args:
+            name: The name to access this cache by `self.get(name)`
+            data_type: The kind of cache to save data to (set, list, dict)
+            path_to_file: An optional file to save cached data to on exit
+            display: Whether to show this in the StatDisplay
+            load:  Whether to load existing data in `path_to_file `into the cache
+            save_kwargs: Kwargs to pass when saving via `FileManager`
+            load_kwargs: Kwargs to pass when loading via `FileManager`
+
+        Returns:
+
+        """
+
+        self.logger.debug('registering "%s" -> "%s"', name, path_to_file)
+        if not display: self.do_not_display.append(name)
+        if save_kwargs: self.save_kwargs[name] = save_kwargs
+        if load_kwargs: self.load_kwargs[name] = load_kwargs
+        else: load_kwargs = {}
+        loaded_data = None
+        if path_to_file:
+            file_path, file_name = os.path.split(path_to_file)
+            if not file_path: file_path = '.'
+            self.filemanager.register_file(file_name, file_path,
+                                           short_name=name)
+
+            if load and self.filemanager.exists(name):
+                loaded_data = self.filemanager.smart_load(name, **load_kwargs)
+                self.logger.debug('loaded data for "%s" -> %s', name,
+                                  (str(loaded_data)[:75] + '...') if len(
+                                          str(loaded_data)) > 75 else str(
+                                          loaded_data))
+        self._create_cache(data_type, loaded_data, name)
+        del loaded_data
+
+    def _create_cache(self, initial_data, loaded_data, name):
+        if isinstance(initial_data, set):
+            if loaded_data: loaded_data = _numericize(loaded_data)
+            self[name] = CacheSet(loaded_data or set(), f'cache/{name}')
+            self.logger.debug('creating new CacheSet for %s', name)
+        elif isinstance(initial_data, list):
+            self[name] = Deque(loaded_data or list(), f'cache/{name}')
+            self.logger.debug('creating new Deque for %s', name)
+        elif isinstance(initial_data, dict):
+            self[name] = Index(f'cache/{name}', **(loaded_data or dict()))
+            self.logger.debug('creating new Index for %s', name)
+
     def load(self, data, loaded_data, name):
         if isinstance(loaded_data, Iterable) and not isinstance(
                 loaded_data, dict):
@@ -75,8 +141,7 @@ class DataManager(UserDict):
         if isinstance(data, set):
             self[name].update(loaded_data)
         elif isinstance(data, list):
-            for d in loaded_data:
-                self[name].append(d)
+            self[name].extend(loaded_data)
         elif isinstance(data, dict):
             self[name].update(loaded_data)
 
@@ -90,7 +155,7 @@ class DataManager(UserDict):
             if k in self.do_not_display:
                 continue
             # only print the length of iterable values
-            if isinstance(v, Sized) and not isinstance(v, str):
+            if isinstance(v, Sized) and not isinstance(v, (str, bytes)):
                 string += f'\t\t\t    {k}: {len(v)}\n'
         string += f'\t\t    <{"END TOTALS".center(55, "-")}>\n'
         return string.rstrip()
@@ -109,3 +174,14 @@ class DataManager(UserDict):
                                             **save_kwargs)
             except Exception as e:
                 self.logger.exception(e)
+
+    def clean(self):
+        """
+        Save storage space by clearing the caches when the data
+        is already stored as a file
+        """
+        for name, value in self.items():
+            if name not in self.filemanager:
+                continue
+            if isinstance(value, (CacheSet, Deque, Index)):
+                value.clear()
