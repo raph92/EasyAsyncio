619f805189031cb735bcade6d2c922161922a376
raphael <rtnanje@gmail.com>
2019-12-23 14:55:54 -0500

    add custom hash function to CacheSet for consistent hash results

--- easyasyncio/cachetypes.py
+++ easyasyncio/cachetypes.py
@@ -1,19 +1,28 @@
-from typing import Iterable
+import hashlib
+from typing import Iterable, Hashable
 
 from diskcache import Index
 
 
 class CacheSet(Iterable):
+    """
+    A Set-like Cache that wraps :class:`diskcache.Index`
+    """
+
     def __init__(self, iterable=(), directory=None):
         self.index = Index(directory)
         self.update(*iterable)
 
     def add(self, obj):
-        self.index[hash(obj)] = obj
+        if not isinstance(obj, Hashable):
+            raise TypeError(f'{type(obj)} is not Hashable',
+                            f'{str(obj)[:100]}...')
+        self.index[self._hash(obj)] = obj
 
     def remove(self, obj):
-        hash_ = hash(obj)
-        if hash_ not in self.index:
+        hash_ = self._hash(obj)
+        index = set(self.index.keys())
+        if hash_ not in index:
             raise KeyError(obj)
         del self.index[hash_]
 
@@ -24,17 +33,42 @@ class CacheSet(Iterable):
         for o in obj:
             self.add(o)
 
+    def clear(self):
+        self.index.clear()
+
     def difference(self, other):
-        set(self) - set(other)
+        self.__sub__(other)
 
     def copy(self):
         return set(self).copy()
+
+    @staticmethod
+    def _hash(obj):
+        """
+        Since hash() is not guaranteed to give the same result in different
+        sessions, we will be using hashlib for more consistent results
+        """
+        hash_id = hashlib.md5()
+        hash_id.update(repr(obj).encode('utf-8'))
+        return str(hash_id.hexdigest())
 
     def __iter__(self):
         return iter(self.index.values())
 
     def __contains__(self, item):
-        return hash(item) in self.index
+        if isinstance(item, set):
+            item = tuple(item)
+        self__hash = self._hash(item)
+        return self__hash in self.index
 
     def __sub__(self, other: Iterable):
         return set(self) - set(other)
+
+    def __len__(self):
+        return len(self.index)
+
+    def __str__(self):
+        return f'CacheSet({", ".join(self)})'
+
+    def __repr__(self):
+        return str(self)
