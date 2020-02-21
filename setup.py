5a6486ae1b7e88bddfabd4d33344f6d2d7d70145
raphael <rtnanje@gmail.com>
2020-02-20 22:57:21 -0500

    v14.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='easyasyncio',
-        version='13.0.0',
+        version='14.0.0',
         packages=['easyasyncio'],
         url='https://github.com/RaphaelNanje/easyasyncio.git',
         license='MIT',
